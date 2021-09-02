d203b4533540aed7af6f0f4f1524d9cf9a333010
KimrAndrew <andrewrk1997@gmail.com>
2021-09-02 11:57:29 -0700

    added interactive elements

---
+++ app1.js
@@ -0,0 +1,8 @@
+console.log("hello world!");
+let coffee = prompt("Do you like coffee?");
+if (coffee.toLowerCase() == "yes"){
+    document.write(<img src="/Users/zeroaku/projects/courses/102/lab04/lab04/fahmi-fakhrudin-nzyzAUsbV0M-unsplash.jpg"/>);
+}
+else {
+    document.write(<img src="https://via.placeholder.com/1400x300"/>)
+}
---
+++ app3.js
@@ -0,0 +1,7 @@
+let answer = prompt("Do you like coffee?");
+if (answer.toLowerCase() == "yes")  {
+    alert("Very Good!")
+}
+else {
+    alert("How tho?")
+}
