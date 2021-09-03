fc483778104a06aa74fbe008588988c487ba1dff
KimrAndrew <andrewrk1997@gmail.com>
2021-09-03 12:09:41 -0700

    added image loop

--- app1.js
+++ app1.js
@@ -2,15 +2,15 @@ function getName() {
     let name = prompt("Please enter your name:");
     document.write(name);
 }
-function wantsCoffee() {
-    let coffee = prompt("Would you like a cup?");
-    if (coffee.toLowerCase() == "yes"){
-        document.write('<img src="https://images.unsplash.com/photo-1518832553480-cd0e625ed3e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80" width="1400"/>');
-    }
-    else {
-        document.write('<img src="https://via.placeholder.com/1400x300"/>')
-    }
-}
+//function wantsCoffee() {
+    //let coffee = prompt("Would you like a cup?");
+    //if (coffee.toLowerCase() == "yes"){
+        //document.write('<img src="https://images.unsplash.com/photo-1518832553480-cd0e625ed3e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80" width="1400"/>');
+    //}
+    //else {
+        //document.write('<img src="https://via.placeholder.com/1400x300"/>')
+    //}
+//}
 function likesCoffee() {
     let answer = prompt("Do you like coffee?");
     if (answer.toLowerCase() == "yes")  {
@@ -18,5 +18,15 @@ function likesCoffee() {
     }
     else {
         alert("How tho?")
+    }
+}
+
+function wantsCoffee() {
+    let coffee = prompt("How many cups would you like");
+    while (coffee < 0 || coffee > 5) {
+        coffee = prompt("How many cups would you like");
+    }
+    for (let i = 0; i < coffee; i++) {
+        document.write('<img src="https://images.unsplash.com/photo-1518832553480-cd0e625ed3e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80" width="1400"/>');
     }
 }
