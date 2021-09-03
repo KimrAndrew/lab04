381bfea826b1f4ec2ca2a3f736e65501cad465b7
KimrAndrew <andrewrk1997@gmail.com>
2021-09-02 21:20:13 -0700

    refactored code to use functions

--- app1.js
+++ app1.js
@@ -1,8 +1,22 @@
-console.log("hello world!");
-let coffee = prompt("Do you like coffee?");
-if (coffee.toLowerCase() == "yes"){
-    document.write(<img src="/Users/zeroaku/projects/courses/102/lab04/lab04/fahmi-fakhrudin-nzyzAUsbV0M-unsplash.jpg"/>);
+function getName() {
+    let name = prompt("Please enter your name:");
+    document.write(name);
 }
-else {
-    document.write(<img src="https://via.placeholder.com/1400x300"/>)
+function wantsCoffee() {
+    let coffee = prompt("Would you like a cup?");
+    if (coffee.toLowerCase() == "yes"){
+        document.write('<img src="/Users/zeroaku/projects/courses/102/lab04/lab04/fahmi-fakhrudin-nzyzAUsbV0M-unsplash.jpg"/>');
+    }
+    else {
+        document.write('<img src="https://via.placeholder.com/1400x300"/>')
+    }
+}
+function likesCoffee() {
+    let answer = prompt("Do you like coffee?");
+    if (answer.toLowerCase() == "yes")  {
+        alert("Very Good!")
+    }
+    else {
+        alert("How tho?")
+    }
 }
