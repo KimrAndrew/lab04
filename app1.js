70f7987f128f8c6e55c5358122bc6dcb61d9d5b3
KimrAndrew <andrewrk1997@gmail.com>
2021-09-02 21:30:07 -0700

    fixed image source

--- app1.js
+++ app1.js
@@ -5,7 +5,7 @@ function getName() {
 function wantsCoffee() {
     let coffee = prompt("Would you like a cup?");
     if (coffee.toLowerCase() == "yes"){
-        document.write('<img src="fahmi-fakhrudin-nzyzAUsbV0M-unsplash.jpg"/>');
+        document.write('<img src="https://images.unsplash.com/photo-1518832553480-cd0e625ed3e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80"/>');
     }
     else {
         document.write('<img src="https://via.placeholder.com/1400x300"/>')
